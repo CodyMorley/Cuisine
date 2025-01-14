1a9cacaff4bfc2daed44cee3c70dcafc11fa343b
Morley <codymorleyinc@gmail.com>
2025-01-14 08:54:38 -0800

    Tests - ImageCache, Recipe Model, Application Main
    Set up testing for all current business logic.

--- Cuisine/CuisineTests/CuisineTests.swift
+++ Cuisine/CuisineTests/CuisineTests.swift
@@ -5,32 +5,17 @@
 //  Created by Cody Morley on 1/14/25.
 //
 
+import SwiftUI
 import XCTest
 @testable import Cuisine
 
 final class CuisineTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testRecipesAppBody() {
+        let app = CuisineApp()
+        
+        let body = app.body
+        
+        XCTAssertNotNil(body, "The app's body should not be nil")
+        XCTAssertTrue(body is WindowGroup<ContentView>, "The body should be a WindowGroup containing ContentView")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }

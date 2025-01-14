a574ad7822b01cbeb96d2c2b7b4a6a64c7956b12
Morley <codymorleyinc@gmail.com>
2025-01-14 09:18:37 -0800

    Recipe - Network
    Built fetch service for recipe data. Added additional protocols and child-classes for testability.

---
+++ Cuisine/Cuisine/Features/Recipe/Network/RecipeFetchService.swift
@@ -0,0 +1,65 @@
+//
+//  RecipeFetchService.swift
+//  Cuisine
+//
+//  Created by Cody Morley on 1/14/25.
+//
+
+import Foundation
+
+
+protocol RecipeFetching {
+    func fetch() async throws -> [Recipe]
+}
+
+
+class RecipeFetchService: RecipeFetching {
+    let session: URLSession
+    
+    
+    init(session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    
+    func fetch() async throws -> [Recipe] {
+        guard let url = constructURL() else { throw URLError(.badURL) }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let (data, response) = try await session.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse else { throw URLError(.badServerResponse) }
+        
+        guard  200..<300 ~= httpResponse.statusCode else {
+            print("Response Code: \(httpResponse.statusCode)")
+            throw URLError(.badServerResponse)
+        }
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            let recipeResponse = try decoder.decode(RecipeResponse.self, from: data)
+            switch recipeResponse.recipes.isEmpty {
+            case true: throw URLError(.resourceUnavailable)
+            case false: return recipeResponse.recipes
+            }
+        } catch {
+            throw error
+        }
+    }
+    
+    fileprivate func constructURL() -> URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "d3jbb8n5wk0qxi.cloudfront.net"
+        components.path = "/recipes.json"
+        return components.url
+    }
+}
+
+
+class RecipeFetchServiceInvalidURL: RecipeFetchService {
+    override func constructURL() -> URL? { nil }
+}

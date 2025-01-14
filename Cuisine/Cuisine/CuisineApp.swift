778aa799066abf138ed3c317a72dd85d0d008c42
Morley <codymorleyinc@gmail.com>
2025-01-14 08:12:05 -0800

    Empty project
    Initial project file

---
+++ Cuisine/Cuisine/CuisineApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuisineApp.swift
+//  Cuisine
+//
+//  Created by Cody Morley on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct CuisineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
